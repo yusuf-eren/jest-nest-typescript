166710c0a2b17afec4e7fe3129162d92a9befbc1
yusuf-eren <erenyusuf170@gmail.com>
2022-10-12 03:27:34 +0300

    utils

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,4 +1,10 @@
+import { UrlWithParsedQuery, parse } from "url"
+
 export class Utils {
+    public static parseUrl(url: string): UrlWithParsedQuery {
+        return parse(url, true)
+    }
+
     public static toUpperCase(arg: string) {
         return arg.toUpperCase()
     }

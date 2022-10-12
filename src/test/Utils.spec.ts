2aedb8fa0dafb5d6a5802ed5352d393be856d626
yusuf-eren <erenyusuf170@gmail.com>
2022-10-12 03:48:15 +0300

    try catch test / arrow function test

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,46 +1,70 @@
-import { Utils } from "../app/Utils"
+import { Utils } from "../app/Utils";
 
 // describe.only() Runs only this test
 // describe.skip() Skips this test
 describe("Utils test suite", () => {
     // beforeEach runs every time before running any test
     beforeEach(() => {
-        console.log("before each")
-    })
+        console.log("before each");
+    });
 
     // beforeAll runs once. It runs at the start of the test
     beforeAll(() => {
-        console.log("before All")
-    })
+        console.log("before All");
+    });
 
     // we can use skip() and only() for "test" too
     test.skip("to upper case test", () => {
-        const result = Utils.toUpperCase("abc")
-        expect(result).toBe("ABC")
-    })
+        const result = Utils.toUpperCase("abc");
+        expect(result).toBe("ABC");
+    });
 
     // test.only()
     test("parse simple URL", () => {
-        const url = "http://localhost:3000/x%20y"
-        const parsedUrl = Utils.parseUrl(url)
-        expect(parsedUrl.port).toBe("3000")
-        expect(parsedUrl.protocol).toBe("http:")
-        expect(parsedUrl.query).toEqual({})
-    })
+        const url = "http://localhost:3000/x%20y";
+        const parsedUrl = Utils.parseUrl(url);
+        expect(parsedUrl.port).toBe("3000");
+        expect(parsedUrl.protocol).toBe("http:");
+        expect(parsedUrl.query).toEqual({});
+    });
 
     test("parse URL with query", () => {
-        const url = "http://localhost:3000/login?user=yusuf&password=1234"
-        const parsedUrl = Utils.parseUrl(url)
+        const url = "http://localhost:3000/login?user=yusuf&password=1234";
+        const parsedUrl = Utils.parseUrl(url);
         const expectedQuery = {
             user: "yusuf",
             password: "1234",
-        }
-        expect(parsedUrl.query).toEqual(expectedQuery)
-        expect(typeof parsedUrl.query.user).toBe("string")
-    })
+        };
+        expect(parsedUrl.query).toEqual(expectedQuery);
+        expect(typeof parsedUrl.query.user).toBe("string");
+    });
 
     // todo() marks this test as todo
     // so we can check this test later
     // and fix the problems
-    test.todo("invalid URL")
-})
+    test.todo("MARK AS TODO TEST");
+
+    test("test invalid URL", () => {
+        function expectError() {
+            Utils.parseUrl("");
+        }
+        // if URL is invalid we are throwing
+        // an error from Utils.ts
+        expect(expectError).toThrowError("");
+    });
+
+    test("test invalid URL with arrow function", () => {
+        expect(() => {
+            Utils.parseUrl("");
+        }).toThrow("Empty URL!");
+    });
+
+    test.only("test invalid URL with try catch", () => {
+        try {
+            Utils.parseUrl("");
+        } catch (err) {
+            expect(err).toBeInstanceOf(Error);
+            expect(err).toHaveProperty("message", "Empty URL!");
+        }
+    });
+});
